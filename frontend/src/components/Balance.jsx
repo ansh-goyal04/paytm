ee87738ca89c9ee9584c0f357febb19f9bd4afe5
ansh goyal <anshmailbox2004@gmail.com>
2024-07-29 09:15:22 +0530

    ..

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -2,10 +2,11 @@
 export default function Balance({ value }){
     return <div className="flex">
         <div className="font-bold text-lg">
-            Your balance
+            Your balance :
         </div>
         <div className="font-semibold ml-4 text-lg">
-            Rs {value}
+            
+            Rs {Math.floor(value)}
         </div>
     </div>
 }
